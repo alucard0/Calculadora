9b942441cd7bab05f68278c6b010dafc7d57f317
Amilkhael <amil@enconta.com>
2021-02-24 23:49:07 -0600

    arreglar y agregar base de los estilos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 import React from 'react'
 import './assets/scss/main.scss'
+import Result from './components/Result'
 
-const App = () => <h1 className="test">HOla M</h1>
+const App = () => {
+  return (
+    <div className="calculator">
+      <Result />
+      <div className="numbers">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>0</button>
+      </div>
+      <div className="functions">
+        <button>clear</button>
+        <button>r</button>
+      </div>
+      <div className="math-operations">
+        <button>+</button>
+        <button>-</button>
+        <button>*</button>
+        <button>/</button>
+        <button>=</button>
+      </div>
+    </div>
+  )
+}
 
 export default App
