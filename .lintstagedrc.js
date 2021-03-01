03d591ead7a0dd5e27383430d1b0553b46aeaab4
Amilkhael <amil@enconta.com>
2021-02-28 21:24:21 -0600

    resumir funciones

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   "./src/*.{js,jsx,ts,tsx}": [
     "npx prettier --write",
-    "eslint src/*.js --fix-dry-run"
+    "eslint src/*.jsx --fix-dry-run"
   ],
   "*.scss": [
     "stylelint --fix --syntax=scss",
