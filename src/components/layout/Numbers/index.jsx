82594cb8cf45dab54b414582e216cc790239253c
Amilkhael <amil@enconta.com>
2021-03-01 00:05:43 -0600

    eslint

--- src/components/layout/Numbers/index.jsx
+++ src/components/layout/Numbers/index.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
-import PropTypes  from 'prop-types'
+import PropTypes from 'prop-types'
 import Button from '../../shared/Button'
 
-const renderButtons = clickNumber => {
-  const numbers = [1,2,3,4,5,6,7,8,9,0]
-  
-  return numbers.map( number => <Button text={`${number}`} clickHandler={clickNumber} key={number} />)
+const renderButtons = (clickNumber) => {
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+  return numbers.map((number) => (
+    <Button text={`${number}`} clickHandler={clickNumber} key={number} />
+  ))
 }
 
-const Numbers = ({clickNumber}) => <div className="calculator__numbers">{renderButtons(clickNumber)}</div>
+const Numbers = ({ clickNumber }) => (
+  <div className="calculator__numbers">{renderButtons(clickNumber)}</div>
+)
 
-Numbers.propTypes ={
-  clickNumber: PropTypes.func.isRequired
+Numbers.propTypes = {
+  clickNumber: PropTypes.func.isRequired,
 }
 
-Numbers.defaultProps ={
-  clickNumber: ()=>{}
- }
-
+Numbers.defaultProps = {
+  clickNumber: () => {},
+}
 
 export default Numbers
