56fe0b96a90c8746ea2fd893a678ea0221df2755
Amilkhael <amil@enconta.com>
2021-03-01 00:06:08 -0600

    eslint

--- src/components/layout/MathOperations/index.jsx
+++ src/components/layout/MathOperations/index.jsx
@@ -2,30 +2,29 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Button from '../../shared/Button'
 
-
-const renderButtons =(clickOperation, clickEqual) =>{
-  const symbols = ["+","-","*","/","="]
-  return symbols.map(symbol=> {
-    if(symbol==='='){
-      return <Button text={symbol} clickHandler={clickEqual} key={symbol}/>
+const renderButtons = (clickOperation, clickEqual) => {
+  const symbols = ['+', '-', '*', '/', '=']
+  return symbols.map((symbol) => {
+    if (symbol === '=') {
+      return <Button text={symbol} clickHandler={clickEqual} key={symbol} />
     } else {
-      return <Button text={symbol}clickHandler={clickOperation} key={symbol} />
+      return <Button text={symbol} clickHandler={clickOperation} key={symbol} />
     }
   })
 }
-const MathOperations =({clickOperation, clickEqual}) => (
+const MathOperations = ({ clickOperation, clickEqual }) => (
   <section className="calculator__math-operations">
     {renderButtons(clickOperation, clickEqual)}
   </section>
 )
 
-MathOperations.propTypes ={
-  clickOperation: PropTypes.func.isRequired, 
-  clickEqual: PropTypes.func.isRequired
+MathOperations.propTypes = {
+  clickOperation: PropTypes.func.isRequired,
+  clickEqual: PropTypes.func.isRequired,
 }
- MathOperations.defaultProps ={
-  clickOperation: ()=>{},
-  clickEqual: ()=>{}
- }
+MathOperations.defaultProps = {
+  clickOperation: () => {},
+  clickEqual: () => {},
+}
 
- export default MathOperations
+export default MathOperations
