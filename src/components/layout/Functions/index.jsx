6d8d35a87448f9ac79c67f1aed5204806acb2a98
Amilkhael <amil@enconta.com>
2021-02-28 23:01:22 -0600

    functions componente

--- src/components/layout/Functions/index.jsx
+++ src/components/layout/Functions/index.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Button from '../../shared/Button'
 
-const Functions =({onContentClear, onDelete})=>(
+const Functions = ({ onContentClear, onDelete }) => (
   <section className="calculator__functions">
-    <Button  text='clear' clickHandler={onContentClear} />
-    <Button  text='r' clickHandler={onDelete}/>
+    <Button text="clear" clickHandler={onContentClear} />
+    <Button text="r" clickHandler={onDelete} />
   </section>
 )
 
-Functions.propTypes={
-  onContentClear: PropTypes.func.isRequired, 
-  onDelete: PropTypes.func.isRequired
+Functions.propTypes = {
+  onContentClear: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
 }
 
 export default Functions
