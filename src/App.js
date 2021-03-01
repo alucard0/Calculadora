245146260e7c85321b17205ac756e5b390cf7580
Amilkhael <amil@enconta.com>
2021-02-28 23:00:55 -0600

    functions componente

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './assets/scss/main.scss'
 import Result from './components/shared/Result'
 import Numbers from './components/layout/Numbers'
 import MathOperations from './components/layout//MathOperations'
+import Functions from './components/layout/Functions'
 
 const clickOperation = (operation) => {
   console.log('Operation', operation)
@@ -12,16 +13,21 @@ const clickEqual = (equal) => {
   console.log('Equal', equal)
 }
 
+const onContentClear = () => {
+  console.log('Clear')
+}
+
+const onDelete = () => {
+  console.log('Delete')
+}
+
 const App = () => {
   return (
     <div className="calculator">
       <Result value={'0'} />
       <Numbers />
 
-      <div className="calculator__functions">
-        <button>clear</button>
-        <button>r</button>
-      </div>
+      <Functions onContentClear={onContentClear} onDelete={onDelete} />
       <MathOperations clickOperation={clickOperation} clickEqual={clickEqual} />
     </div>
   )
---
+++ src/components/layout/Functions/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Button from '../../shared/Button'
+
+const Functions =({onContentClear, onDelete})=>(
+  <section className="calculator__functions">
+    <Button  text='clear' clickHandler={onContentClear} />
+    <Button  text='r' clickHandler={onDelete}/>
+  </section>
+)
+
+Functions.propTypes={
+  onContentClear: PropTypes.func.isRequired, 
+  onDelete: PropTypes.func.isRequired
+}
+
+export default Functions
