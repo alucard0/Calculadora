d9c037e70d81d9473f17acc2b557f7ef98b93a4b
Amilkhael <amil@enconta.com>
2021-03-03 21:52:01 -0600

    usar hooks

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
-import React from 'react'
+/* eslint no-eval: 0 */
+import React, { useState } from 'react'
 import './assets/scss/main.scss'
 import Result from './components/shared/Result'
 import Numbers from './components/layout/Numbers'
 import MathOperations from './components/layout//MathOperations'
 import Functions from './components/layout/Functions'
 
-const clickOperation = (operation) => {
-  console.log('Operation', operation)
-}
-
-const clickEqual = (equal) => {
-  console.log('Equal', equal)
-}
-
-const onContentClear = () => {
-  console.log('Clear')
-}
-
-const onDelete = () => {
-  console.log('Delete')
-}
-
-const clickNumber = (text) => {
-  console.log('Button', text)
-}
-
 const App = () => {
+  const [stack, setStack] = useState('')
+
+  const clickNumber = (number) => {
+    setStack(`${stack}${number}`)
+  }
+
+  const clickOperation = (operation) => {
+    setStack(`${stack}${operation}`)
+  }
+
+  const clickEqual = (equal) => {
+    console.log('Equal', equal)
+    const result = eval(stack)
+    setStack(`${result}`)
+  }
+
+  const onContentClear = () => {
+    setStack('')
+  }
+
+  const onDelete = () => {
+    if (stack.length > 0) {
+      const newStack = stack.substring(0, stack.length - 1)
+      setStack(newStack)
+    }
+  }
+
   return (
     <div className="calculator">
-      <Result value={'0'} />
+      <Result value={stack} />
       <Numbers clickNumber={clickNumber} />
       <Functions onContentClear={onContentClear} onDelete={onDelete} />
       <MathOperations clickOperation={clickOperation} clickEqual={clickEqual} />
